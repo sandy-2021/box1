bafb2bd104de4d68bafd88b9b331ecb87e41190b
sandy-2021 <92208303+sandy-2021@users.noreply.github.com>
2021-10-09 15:19:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box;
+function setup() {
+  createCanvas(400,400); 
+box=createSprite(200,200,30,30);
+box.shapeColor="red";
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+}
+
+
+
+
